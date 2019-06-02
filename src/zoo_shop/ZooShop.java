4dbfac0f13f2ab9bb884bd5e182f0f9d2bb6434e
BagerManBG <toshko_1998@abv.bg>
2019-06-02 21:51:22 +0300

    Added form layout for adding items

--- src/zoo_shop/ZooShop.java
+++ src/zoo_shop/ZooShop.java
@@ -1,8 +1,11 @@
 package zoo_shop;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 public class ZooShop {
     private JTabbedPane tabbed_pane_main;
@@ -10,13 +13,61 @@ public class ZooShop {
     private JTextField text_field_species_name;
     private JButton button_add_species;
     private JScrollPane scroll_pane_species_list;
+    private JTextField text_field_animals_name;
+    private JTextField text_field_animals_age;
+    private JTextField text_field_animals_price;
+    private JComboBox combo_box_animals_species;
+    private JButton button_add_animals;
+    private JScrollPane scroll_pane_animals_list;
+    private JScrollPane scroll_pane_clients_list;
+    private JTextField text_field_clients_name;
+    private JTextField text_field_clients_email;
+    private JTextField text_field_clients_phone;
+    private JButton button_add_clients;
+    private JScrollPane scroll_pane_adoptions_list;
+    private JComboBox combo_box_adoptions_client;
+    private JComboBox combo_box_adoptions_animal;
+    private JButton button_add_adoptions;
 
     public ZooShop() {
+        // On Tab Change Listener
+        tabbed_pane_main.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                //JOptionPane.showMessageDialog(new JFrame("ZooShop"), "Hallo");
+            }
+        });
+
+        // On Add Specie Button Click
         button_add_species.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String name = ZooShop.this.text_field_species_name.getText();
                 JOptionPane.showMessageDialog(new JFrame("ZooShop"), name);
+            }
+        });
+
+        // On Add Animal Button Click
+        button_add_animals.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        // On Add Client Button Click
+        button_add_clients.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        // On Add Adoption Button Click
+        button_add_adoptions.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
             }
         });
     }
@@ -26,7 +77,7 @@ public class ZooShop {
         frame.setContentPane(new ZooShop().panel_main);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setSize(800, 800);
+        frame.setSize(1000, 800);
 
         frame.pack();
         frame.setVisible(true);
